e22e1b347a40c4f320f181300599d13b26ee1827
markk <mark.karchmit@yandex.ru>
2024-11-14 14:12:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/effectivetest/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.effectivetest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.effectivetest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+   private  lateinit var  binding :ActivityMainBinding
+    private val navController: NavController by lazy {
+        findNavController(R.id.nav_host_fragment)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setupWithNavController(navController)
+
+        //val navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
